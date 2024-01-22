6242f83931424027db90ccdc0fbf87ba8e4f2035
Himanshu Tomar <himanshutomar3031@gmail.com>
2024-01-22 13:21:39 +0530

    updated a file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn SSO
+          Learn SSO Azure
         </a>
       </header>
     </div>
