7bed11b3f952a607a78cdfc2b87123e3421120d4
Himanshu Tomar <himanshutomar3031@gmail.com>
2024-01-23 16:08:36 +0530

    updated the code

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { useEffect } from "react";
 import { useMsal } from "@azure/msal-react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const { instance } = useMsal();
+
 function App() {
+  const { instance } = useMsal();
   const getAccessToken = async () => {
 
     instance
