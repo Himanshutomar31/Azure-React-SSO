e6459452d60059229288d3db2507f9bc1e4954ad
aswini <gvc.ashwini@gmail.com>
2024-01-23 15:40:27 +0530

    removed imported configuration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { MsalProvider } from "@azure/msal-react";
-import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from "@azure/msal-browser";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
