68876cc11200dc5392974b4a3aba9414e1720c93
Himanshu Tomar <himanshutomar3031@gmail.com>
2024-01-23 16:17:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,26 +8,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const { instance } = useMsal();
-  const getAccessToken = async () => {
-
-    instance
-    .acquireTokenSilent({
-      scopes: ["user.read"],
-      authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
-    })
-    .then((response) => {
-      console.log(response)
-      const headers = new Headers();
-      headers.append("Authorization", `Bearer ${response.accessToken}`);
-      return fetch(`https://graph.microsoft.com/v1.0/me`, {
-        method: "GET",
-        headers,
-      }).then((response) => console.log(response.json()))
-    });    
-
-  };
 
   useEffect(() => {
+    const getAccessToken = async () => {
+      instance
+      .acquireTokenSilent({
+        scopes: ["user.read"],
+        authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
+      })
+      .then((response) => {
+        console.log(response)
+        const headers = new Headers();
+        headers.append("Authorization", `Bearer ${response.accessToken}`);
+        return fetch(`https://graph.microsoft.com/v1.0/me`, {
+          method: "GET",
+          headers,
+        }).then((response) => console.log(response.json()))
+      });    
+  
+    };
     getAccessToken();
   }, []);
   return (
