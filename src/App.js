ba4328ee896f8c5f4eecc35d4fdd4cbafb1a82e2
aswini <gvc.ashwini@gmail.com>
2024-01-23 15:38:19 +0530

    added function for getting access token

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.css";
-import Home from "./Home.js"
+import Home from "./Home.js";
+import axios from "axios";
+import { useEffect } from "react";
 
-import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const getAccessToken = async () => {
+    let response = await axios.get("https://graph.microsoft.com/v1.0/me");
+    console.log("response", response);
+    return response;
+  };
 
-
-  return (<Router><Routes>
-    <Route path="/home" element={<Home></Home>}></Route>
-    <Route path="/" element={<h1>Hello world!</h1>}></Route>
-    </Routes></Router>)
+  useEffect(() => {
+    getAccessToken().then((res) => {
+      console.log("access token", res);
+    });
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/" element={<h1>Hello world!</h1>}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { MsalProvider } from "@azure/msal-react";
-import { Configuration,  PublicClientApplication } from "@azure/msal-browser";
+import { Configuration, PublicClientApplication } from "@azure/msal-browser";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const msalConfiguration = {
-    auth: {
-        clientId: "client_id" 
-    }
+  auth: {
+    clientId: "31a0fdca-b2f7-4d01-bb76-8d681ca5487c",
+  },
 };
 const pca = new PublicClientApplication(msalConfiguration);
 
 root.render(
-<MsalProvider instance={pca}>
-       <App />
-</MsalProvider>
+  <MsalProvider instance={pca}>
+    <App />
+  </MsalProvider>
 );
-
-
