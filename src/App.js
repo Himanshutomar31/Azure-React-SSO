52368c432ff89a47bec3e7db3bf18680909c3772
aswini <gvc.ashwini@gmail.com>
2024-01-23 18:42:11 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import "./App.css";
 import Home from "./Home.js";
 // import axios from "axios";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 import { useMsal } from "@azure/msal-react";
-
+import {
+  // AuthenticatedTemplate,
+  // UnauthenticatedTemplate,
+  useMsal,
+  // useMsalAuthentication,
+} from "@azure/msal-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const { instance } = useMsal();
-  useEffect(() => {
-    const getAccessToken = async () => {
-      instance
-        .acquireTokenSilent({
-          scopes: ["user.read"],
-          authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
-        })
-        .then((response) => {
-          console.log(response);
-          const headers = new Headers();
-          headers.append("Authorization", `Bearer ${response.accessToken}`);
-          return fetch(`https://graph.microsoft.com/v1.0/me`, {
-            method: "GET",
-            headers,
-          }).then((response) => console.log(response.json()));
-        });
-    };
-    getAccessToken();
-  }, [instance]);
+  const { accounts } = useMsal();
+  const user = accounts[0];
+  // useEffect(() => {
+  //   const getAccessToken = async () => {
+  //     instance
+  //       .acquireTokenSilent({
+  //         scopes: ["user.read"],
+  //         authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
+  //       })
+  //       .then((response) => {
+  //         console.log(response);
+  //         const headers = new Headers();
+  //         headers.append("Authorization", `Bearer ${response.accessToken}`);
+  //         return fetch(`https://graph.microsoft.com/v1.0/me`, {
+  //           method: "GET",
+  //           headers,
+  //         }).then((response) => console.log(response.json()));
+  //       });
+  //   };
+  //   getAccessToken();
+  // }, [instance]);
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home></Home>}></Route>
-        <Route path="/" element={<h1>Hello world!</h1>}></Route>
+        <Route
+          path="/"
+          element={<h1>Hello world! {console.log("user", user)}</h1>}
+        ></Route>
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -10,8 +10,18 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const msalConfiguration = {
   auth: {
     clientId: "31a0fdca-b2f7-4d01-bb76-8d681ca5487c",
+    authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
+    s,
   },
 };
+
+// export const loginRequest = {
+//   scopes: ["User.Read"],
+// };
+
+// export const graphConfig = {
+//   graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+// };
 const pca = new PublicClientApplication(msalConfiguration);
 
 root.render(
