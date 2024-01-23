c5cac5439064ca4a9fc1ecfb352dc42dd3a1af1d
Himanshu Tomar <himanshutomar3031@gmail.com>
2024-01-23 16:04:06 +0530

    updated the code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 import Home from "./Home.js";
 import axios from "axios";
 import { useEffect } from "react";
+import { useMsal } from "@azure/msal-react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const getAccessToken = async () => {
-    let response = await axios.get("https://graph.microsoft.com/v1.0/me");
-    console.log("response", response);
-    return response;
+    const { instance } = useMsal();
+    instance
+    .acquireTokenSilent({
+      scopes: ["user.read"],
+      authority: `https://login.microsoftonline.com/a57f7d92-038e-4d4c-8265-7cd2beb33b34`,
+    })
+    .then((response) => {
+      console.log(response)
+      const headers = new Headers();
+      headers.append("Authorization", `Bearer ${response.accessToken}`);
+      return fetch(`https://graph.microsoft.com/v1.0/me`, {
+        method: "GET",
+        headers,
+      }).then((response) => response.json())
+    });    
+
   };
 
   useEffect(() => {
