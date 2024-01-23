689c67964e016f3731157dd9bdc250766630ceed
Himanshu Tomar <himanshutomar3031@gmail.com>
2024-01-23 14:57:54 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
-import axios from "axios";
+import Home from "./Home.js"
+// import { useEffect } from "react";
+// import axios from "axios";
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
 
 function App() {
-  const getAccessToken = async () => {
-    const response = await axios.get("https://test22012024.azurewebsites.net/.auth/me", {
-      headers: {
-        "Access-Control-Allow-Origin": "*", // Replace * with the actual origin of your frontend
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });   
-    console.log("response =>", response);
-    return response;
-  };
-  useEffect(() => {
-    getAccessToken.then((res) => {
-      console.log("access token=>", res);
-    });
-  },[]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn SSO Azure App Service.
-        </a>
-      </header>
-    </div>
-  );
+  // const getAccessToken = async () => {
+  //   const response = await axios.get("https://test22012024.azurewebsites.net/.auth/me", {
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*", // Replace * with the actual origin of your frontend
+  //       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  //       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  //     },
+  //   });   
+  //   console.log("response =>", response);
+  //   return response;
+  // };
+  // useEffect(() => {
+  //   getAccessToken.then((res) => {
+  //     console.log("access token=>", res);
+  //   });
+  // },[]);
+
+  return (<Router><Routes>
+    <Route path="/home" element={<Home></Home>}></Route>
+    <Route path="/" element={<h1>Hello world!</h1>}></Route>
+    </Routes></Router>)
 }
 
 export default App;
